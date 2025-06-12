0d2f48b1428947390c2a313b845bf2a519d0523b
Gamandorf <lorenzodiraco1994@gmail.com>
2025-06-12 11:52:30 +0200

    classiphp1

---
+++ 1.php
@@ -0,0 +1,16 @@
+<?php
+$companyEmployees = 0;
+class Companies
+{
+    public string $name;
+    public string $site;
+    public int $companyEmployees;
+
+    public function __construct($name, $site, $companyEmployees)
+    {
+        $nEmployee = 0;
+        $this->name = $name;
+        $this->site = $site;
+        $this->companyEmployees = $companyEmployees;
+    }
+}
