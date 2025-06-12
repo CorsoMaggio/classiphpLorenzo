f067a3b4abd5cd1160d1bbc8b9075882dc167672
Gamandorf <lorenzodiraco1994@gmail.com>
2025-06-12 12:27:50 +0200

    classiphp1

--- 1.php
+++ 1.php
@@ -13,3 +13,11 @@ class Company
         $this->companyEmployees = $companyEmployees;
     }
 }
+
+$company1 = new Company('Apple', 'Usa', '5');
+$company2 = new Company('Olivetti', 'Italia', '0');
+$company3 = new Company('Telefunken', 'Germania', '2');
+$company4 = new Company('atet', 'canada', '6');
+$company5 = new Company('xiaomi', 'Cina', '9');
+
+print_r($company4);
