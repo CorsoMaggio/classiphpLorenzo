47004af2d61e85d88d49c2b014d0fbe7549d24e2
Gamandorf <lorenzodiraco1994@gmail.com>
2025-06-12 12:04:51 +0200

    classiphp1

--- 1.php
+++ 1.php
@@ -1,14 +1,13 @@
 <?php
-$companyEmployees = 0;
 class Company
 {
     public string $name;
     public string $site;
     public int $companyEmployees;
 
-    public function __construct($name, $site, $companyEmployees)
+    public function __construct($name, $site, $companyEmployees = 0)
     {
-        $nEmployee = 0;
+
         $this->name = $name;
         $this->site = $site;
         $this->companyEmployees = $companyEmployees;
